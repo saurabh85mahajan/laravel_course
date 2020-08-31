f82f7ca5356563813776fde1cf3c5f4e25796995
Saurabh M <saurabh85mahajan@gmail.com>
2020-08-31 17:42:30 +0530

    Validatio Rules & Error Messages

--- app/Http/Requests/StoryRequest.php
+++ app/Http/Requests/StoryRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoryRequest extends FormRequest
 {
@@ -23,11 +24,34 @@ class StoryRequest extends FormRequest
      */
     public function rules()
     {
+        $storyId = $this->route('story.id');
         return [
-            'title' => 'required',
-            'body' => 'required',
+            'title' => ['required', 'min:10', 'max:50',
+            function ($attribute, $value, $fail) {
+                if ($value == 'Dummy Title') {
+                    $fail($attribute . ' is not Valid');
+                }
+            },
+            Rule::unique('stories')->ignore($storyId)
+        ],
+            'body' => ['required', 'min:50'],
             'type' => 'required',
             'status' => 'required',
+        ];
+    }
+
+    public function withValidator($v)
+    {
+        $v->sometimes('body', 'max:200', function ($input) {
+            return 'short' == $input->type;
+        });
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'You must enter :attribute',
+            'required' => 'Please enter :attribute',
         ];
     }
 }
