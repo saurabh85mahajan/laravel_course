30a63c69f5eee3ae42f4d6df006205a9f1da3045
Saurabh M <saurabh85mahajan@gmail.com>
2020-08-31 17:02:23 +0530

    Eloquent Models

---
+++ app/Story.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Story extends Model
+{
+    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'body', 'type', 'status'
+    ];
+
+    // protected $guarded = [];
+
+    public function user() 
+    {
+        return $this->belongsTo(\App\User::class);
+    }
+
+}
