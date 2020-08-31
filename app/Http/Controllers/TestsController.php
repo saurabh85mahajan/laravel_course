4e2557308aa3f8034d5213a9cf7d5b222e092be0
Saurabh M <saurabh85mahajan@gmail.com>
2020-08-31 16:41:23 +0530

    Database

--- app/Http/Controllers/TestsController.php
+++ app/Http/Controllers/TestsController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TestsController extends Controller
 {
     //
     public function one()
     {
+        $results = DB::table('products')
+            ->where('id', '4')
+            ->value('name');
+        // ->get();
+
+        dd($results);
         return view('tests.one', [
             'foo' => 'bar1',
             'haystack' => 'needle'
