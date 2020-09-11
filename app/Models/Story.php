a75b5617f83b2e887e81cbb4721dd8dea9396309
Saurabh M <saurabh85mahajan@gmail.com>
2020-09-11 14:04:00 +0530

    Laravel 8 Update to Model Factories

--- app/Models/Story.php
+++ app/Models/Story.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Story extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, HasFactory;
     //
     /**
      * The attributes that are mass assignable.
--- database/factories/StoryFactory.php
+++ database/factories/StoryFactory.php
@@ -1,24 +1,41 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+namespace Database\Factories;
 
 use App\Models\Story;
-use Faker\Generator as Faker;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-$factory->define(Story::class, function (Faker $faker) {
-    $type = $faker->randomElement(['long', 'short']);
-    if( $type == 'long') {
-        $body = $faker->paragraph();
-    } else {
-        $body = $faker->text(200);
+class StoryFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Story::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $type = $this->faker->randomElement(['long', 'short']);
+        if( $type == 'long') {
+            $body = $this->faker->paragraph();
+        } else {
+            $body = $this->faker->text(200);
+        }
+        return [
+            //
+            'user_id' => User::factory(),
+            'title' => $this->faker->unique()->lexify('??????????'),
+            'body' => $body,
+            'type' => $type,
+            'status' => $this->faker->boolean()
+        ];
     }
-    return [
-        //
-        'user_id' => $faker->numberBetween(1, 2),
-        'title' => $faker->unique()->lexify('??????????'),
-        'body' => $body,
-        'type' => $type,
-        'status' => $faker->boolean()
-    ];
-});
+}
